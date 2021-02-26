8a8a3e504cf13b1b0aeb9581dbb3e8a8c5c688c9
elias <elias.zankl@gmx.de>
2021-02-26 20:05:05 +0100

    basic components and layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import { Header } from 'Header'
+import { Footer } from 'Footer'
+import { Content } from 'Content'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    Header,
+    Content,
+    Footer,
+  },
+}
+</script>
+
+<style>
+.container {
+  display: grid;
+  grid-template-rows: 20% auto 15%;
+  grid-template-columns: 10% auto 10%;
+}
+</style>
